8de1ab5221d1dd7f4a10e257b0885a86b95251ab
zhouke <zhou.ke@advantech.com.cn>
2018-02-13 10:41:09 +0800

    init

---
+++ express/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zhou.ke
+ * Date: 2018/2/12
+ * Time: 15:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace express
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+        #region
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(24, 335);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "拒绝";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(546, 370);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "Express";
+			this.Load += new System.EventHandler(this.FormLoad);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+		//private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+	}
+}
---
+++ express/MainForm.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zhou.ke
+ * Date: 2018/2/12
+ * Time: 15:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Media;
+	
+namespace express
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	/// 
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		private void FormLoad(object sender, EventArgs e)
+		{
+			
+		//	 axWindowsMediaPlayer.URL = "爱无反顾.mp3"; 
+		
+		}
+		private void Button1Click(object sender, EventArgs e)
+		{
+			try {
+			SoundPlayer player = new SoundPlayer();
+			player.SoundLocation = Application.StartupPath + "\\我养你啊.wav";
+			player.LoadAsync();
+			player.Play();
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		
+		}
+	}
+}
